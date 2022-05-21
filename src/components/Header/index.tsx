33a00d23b9fbc37388d30fb41d9ae956e9b837a5
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-05-20 23:07:40 -0300

    ➕ title: nav-menu section created in Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,13 @@
 import { Container } from './styles'
-import { LinkedIn, GitHub, Instagram } from '@material-ui/icons'
+import {
+  LinkedIn,
+  GitHub,
+  Instagram,
+  HomeRounded,
+  SchoolRounded,
+  CodeRounded,
+  EmailRounded
+} from '@material-ui/icons'
 
 export const Header = () => {
   return (
@@ -16,6 +24,12 @@ export const Header = () => {
         <div className="line-3"></div>
         <div className="line-4"></div>
         <div className="line-5"></div>
+      </div>
+      <div className="nav-menu">
+        <a className="active" href="#"><HomeRounded /></a>
+        <a href="#skills-experience"><SchoolRounded /></a>
+        <a href="#projects"><CodeRounded /></a>
+        <a href="#contact"><EmailRounded /></a>
       </div>
     </Container>
   )
