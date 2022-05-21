b3bb51c6b993c12b95f5d7f2e19e81933cd3c2a1
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-05-20 21:22:41 -0300

    🎨 title: add styles to Header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  width: 6.25rem;
+  max-width: 6.24rem;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1.25rem;
+
+  position: fixed;
+
+  background: var(--gray-900);
+`
