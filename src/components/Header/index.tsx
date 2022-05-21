7dc2aa02f858ad542f86e70df38ebf2b18dc77f6
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-05-20 21:25:26 -0300

    ➕ title: social-media section created in Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,14 @@
 import { Container } from './styles'
+import { LinkedIn, GitHub, Instagram } from '@material-ui/icons'
 
 export const Header = () => {
   return (
     <Container>
-
+      <div className="social-media">
+        <a href="https://www.linkedin.com/in/jhonatan-gabriel-alves-11b28015b/" target="_blank"><LinkedIn /></a>
+        <a href="https://github.com/JhonatanGAlves" target="_blank"><GitHub /></a>
+        <a href="https://www.instagram.com/jhonatangalves" target="_blank"><Instagram /></a>
+      </div>
     </Container>
   )
 }
