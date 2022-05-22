baa6df77ed07e92be0cb79c7e6e731ec969384bc
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-05-22 13:53:12 -0300

    🌗 title: light and dark theme component created

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,11 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    primary: '#181818',
+    secundary: '#FF8600',
+
+    background: '#242424',
+    text: '#F1F2F6'
+  }
+}
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,11 @@
+export default {
+  title: 'light',
+
+  colors: {
+    primary: '#F1F2F6',
+    secundary: '#5F4BB6',
+
+    background: '#FDFFFC',
+    text: '#242424'
+  }
+}
