d55a2a9155b3d4667ced6773f50a2523c4c2573f
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-05-24 12:21:51 -0300

    🔡 title: passing the texts to translate

---
+++ src/i18n/locales/en-us.js
@@ -0,0 +1,16 @@
+export default {
+  translations: { // Mesmo valor usado nas configurações (defaultNS)
+    hero: {
+      h1: {
+        span: "Hi, I'm"
+      },
+      span: {
+        first: "const stack = 'Front End Developer'",
+        second: "const age = 26",
+        third: "const curiosity = 'I have 7 dogs and 1 cat'",
+        fourth: "const academicEducation = 'Analysis and systems development'",
+      },
+      button: "contact"
+    }
+  }
+}
---
+++ src/i18n/locales/pt-br.js
@@ -0,0 +1,16 @@
+export default {
+  translations: { // Mesmo valor usado nas configurações (defaultNS)
+    hero: {
+      h1: {
+        span: "Olá, sou o"
+      },
+      span: {
+        first: "const stack = 'Desenvolvedor Front End'",
+        second: "const idade = 26",
+        third: "const curiosidade = 'Tenho 7 cachorros e 1 gato'",
+        fourth: "const formacao = 'Análise e Desenvolvimento de Sistemas'",
+      },
+      button: "contato"
+    }
+  }
+}
