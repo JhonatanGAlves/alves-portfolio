17027714459498ce1b0b5f56dd5a47a6ff5a1661
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-05-24 12:33:45 -0300

    ➕ title: import Hero component to App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider, DefaultTheme } from 'styled-components'
 import light from '../src/styles/themes/light'
 import dark from '../src/styles/themes/dark'
 import { usePersistedState } from './utils/usePersistedState'
+import { Hero } from './components/Hero'
 
 export const App = () => {
   const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', light)
@@ -14,6 +15,7 @@ export const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Header toggleTheme={toggleTheme} />
+      <Hero />
       <GlobalStyles />
     </ThemeProvider>
   )
